63aa9cc86ce2b2e77c9304d52b69abb79de801fc
matiasbastino <matiasbastino@gmail.com>
2023-03-26 17:52:04 -0300

    BackEnd

---
+++ BackEnd/src/main/java/com/porfolio/matiasbastino/MatiasbastinoApplication.java
@@ -0,0 +1,13 @@
+package com.porfolio.matiasbastino;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatiasbastinoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatiasbastinoApplication.class, args);
+	}
+
+}
---
+++ BackEnd/src/test/java/com/porfolio/matiasbastino/MatiasbastinoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.porfolio.matiasbastino;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatiasbastinoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
